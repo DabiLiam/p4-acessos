c400f588f4d2f43a0b39651f7be3dde2ddeda4a1
Dabi <davikcfranco@gmail.com>
2024-01-05 12:46:16 -0300

    Acessos P4

---
+++ script.js
@@ -0,0 +1,64 @@
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo');
+let barra = document.querySelector('.barra')
+let logoSpan = document.querySelectorAll('.logo-parts');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
+        logoSpan.forEach((span, index) => {
+            setTimeout(() => {
+                span.classList.add('active')
+            }, (index + 1) * 100);
+        });
+
+        setTimeout(() => {
+            logoSpan.forEach((span, index) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (span + 1) * 50);
+            });
+        }, 2000)
+
+        setTimeout(() => {
+            intro.style.top = '-100vh'
+        }, 2300);
+
+    })
+});
+
+const meuNome = document.querySelector('.meu-nome');
+const botaoVerMais = document.querySelector('button');
+const habilidades = document.querySelectorAll('.escondido');
+const node = document.querySelector('#fundo')
+
+function cliqueVer (e) {
+    habilidades.forEach( (elemento) => {
+        if (elemento.classList.contains('escondido')) {
+            elemento.classList.remove('escondido');
+            elemento.classList.add('visivel');
+            botaoVerMais.innerHTML = 'Ver menos';
+        } else {
+            elemento.classList.add('escondido');
+            elemento.classList.remove('visivel');
+            botaoVerMais.innerHTML = 'Ver mais';
+        }
+    } )
+}
+
+function maisHabilidades (elemento) {
+    elemento.addEventListener('click', cliqueVer)
+}
+
+function typeWriter (nome) {
+    setTimeout( () => {
+    const meuNomeF = nome.split('');
+    meuNome.innerHTML='';
+    meuNomeF.forEach( (letra, index) => {
+        setTimeout(function (){
+            meuNome.innerHTML += letra;
+        }, 150 * index)
+    })}, 1500)
+    
+}
